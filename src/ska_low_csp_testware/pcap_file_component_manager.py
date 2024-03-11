3f3e5baf6d807562e97174e0a0a5318e4704ce43
Sander Ploegsma <sander.ploegsma@topic.nl>
2024-03-11 04:02:49 +0100

    TOP-938: Load PCAP file from component manager

--- src/ska_low_csp_testware/pcap_file_component_manager.py
+++ src/ska_low_csp_testware/pcap_file_component_manager.py
@@ -1,13 +1,34 @@
 # pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,broad-exception-caught
 
 import logging
+import threading
 from typing import Any, Callable
 
-from ska_control_model import CommunicationStatus, TaskStatus
-from ska_tango_base.base import CommunicationStatusCallbackType, TaskCallbackType
+import pandas
+import spead2
+import spead2.recv
+from ska_control_model import CommunicationStatus, PowerState, TaskStatus
+from ska_tango_base.base import CommunicationStatusCallbackType, TaskCallbackType, check_communicating
 from ska_tango_base.executor import TaskExecutorComponentManager
 
+from ska_low_csp_testware.spead import SpeadHeapVisitor, process_pcap_file
+
 __all__ = ["PcapFileComponentManager"]
+
+
+class _ExtractVisibilityMetadata(SpeadHeapVisitor):
+    def __init__(self) -> None:
+        self._metadata = []
+
+    def visit_start_of_stream_heap(self, heap: spead2.recv.Heap, items: dict[str, spead2.Item]) -> None:
+        row = {}
+        for key, item in items.items():
+            row[key] = item.value
+        self._metadata.append(row)
+
+    @property
+    def metadata(self) -> pandas.DataFrame:
+        return pandas.DataFrame(self._metadata)
 
 
 class PcapFileComponentManager(TaskExecutorComponentManager):
@@ -29,16 +50,14 @@ class PcapFileComponentManager(TaskExecutorComponentManager):
         if self.communication_state == CommunicationStatus.DISABLED:
             self._update_communication_state(CommunicationStatus.NOT_ESTABLISHED)
 
-        self.logger.info("Start communicating")
-
+        self._update_component_state(power=PowerState.ON, fault=False)
         self._update_communication_state(CommunicationStatus.ESTABLISHED)
 
     def stop_communicating(self) -> None:
         if self.communication_state == CommunicationStatus.DISABLED:
             return
 
-        self.logger.info("Stop communicating")
-
+        self._update_component_state(power=PowerState.UNKNOWN, fault=None)
         self._update_communication_state(CommunicationStatus.DISABLED)
 
     def on(self, task_callback: TaskCallbackType | None = None) -> tuple[TaskStatus, str]:
@@ -52,3 +71,22 @@ class PcapFileComponentManager(TaskExecutorComponentManager):
 
     def reset(self, task_callback: TaskCallbackType | None = None) -> tuple[TaskStatus, str]:
         return TaskStatus.REJECTED, "Command not supported"
+
+    @check_communicating
+    def load(self, task_callback: TaskCallbackType | None = None) -> tuple[TaskStatus, str]:
+        return self.submit_task(self._load, task_callback=task_callback)
+
+    def _load(  # pylint: disable=unused-argument
+        self,
+        task_callback: TaskCallbackType,
+        task_abort_event: threading.Event,
+    ) -> None:
+        task_callback(status=TaskStatus.IN_PROGRESS)
+        visitor = _ExtractVisibilityMetadata()
+        try:
+            process_pcap_file(self._pcap_file_path, visitor)
+            self._update_component_state(metadata=visitor.metadata, fault=False)
+            task_callback(status=TaskStatus.COMPLETED)
+        except Exception as e:
+            self._update_component_state(fault=True)
+            task_callback(status=TaskStatus.FAILED, exception=e)
--- src/ska_low_csp_testware/pcap_file_device.py
+++ src/ska_low_csp_testware/pcap_file_device.py
@@ -1,36 +1,22 @@
 # pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,broad-exception-caught
 
 import pandas
-import spead2
-import spead2.recv
+from ska_control_model import PowerState, ResultCode
 from ska_tango_base.base import SKABaseDevice
+from ska_tango_base.commands import SubmittedSlowCommand
 from tango.server import attribute, command, device_property
 
 from ska_low_csp_testware.pcap_file_component_manager import PcapFileComponentManager
-from ska_low_csp_testware.spead import SpeadHeapVisitor, process_pcap_file
 
 __all__ = ["PcapFileDevice"]
-
-
-class ExtractVisibilityMetadata(SpeadHeapVisitor):
-    def __init__(self) -> None:
-        self._metadata = []
-
-    def visit_start_of_stream_heap(self, heap: spead2.recv.Heap, items: dict[str, spead2.Item]) -> None:
-        row = {}
-        for key, item in items.items():
-            row[key] = item.value
-        self._metadata.append(row)
-
-    @property
-    def metadata(self) -> pandas.DataFrame:
-        return pandas.DataFrame(self._metadata)
 
 
 class PcapFileDevice(SKABaseDevice):
     pcap_file_path: str = device_property()  # type: ignore
 
-    _metadata: pandas.DataFrame | None = None
+    def __init__(self, *args, **kwargs):
+        self._metadata: pandas.DataFrame | None = None
+        super().__init__(*args, **kwargs)
 
     def create_component_manager(self) -> PcapFileComponentManager:
         return PcapFileComponentManager(
@@ -38,6 +24,22 @@ class PcapFileDevice(SKABaseDevice):
             logger=self.logger,
             communication_state_callback=self._communication_state_changed,
             component_state_callback=self._component_state_changed,
+            metadata=self._metadata,
+        )
+
+    def init_command_objects(self) -> None:
+        super().init_command_objects()
+
+        self.register_command_object(
+            "Load",
+            SubmittedSlowCommand(
+                "Load",
+                self._command_tracker,
+                self.component_manager,
+                "load",
+                callback=None,
+                logger=self.logger,
+            ),
         )
 
     @attribute
@@ -48,9 +50,22 @@ class PcapFileDevice(SKABaseDevice):
         return self._metadata.to_json()
 
     @command
-    def load(self) -> None:
-        self.logger.debug("Start unpacking file")
-        visitor = ExtractVisibilityMetadata()
-        process_pcap_file(self.pcap_file_path, visitor)
-        self._metadata = visitor.metadata
-        self.logger.debug("Finished unpacking file")
+    def Load(self) -> tuple[ResultCode, str]:  # pylint: disable=invalid-name
+        handler = self.get_command_object("Load")
+        return handler()
+
+    def _update_metadata(self, metadata: pandas.DataFrame) -> None:
+        self._metadata = metadata
+        self.push_change_event("metadata", metadata)
+        self.push_archive_event("metadata", metadata)
+
+    def _component_state_changed(
+        self,
+        fault: bool | None = None,
+        power: PowerState | None = None,
+        metadata: pandas.DataFrame | None = None,
+    ) -> None:
+        super()._component_state_changed(fault, power)
+
+        if metadata is not None:
+            self._update_metadata(metadata)
