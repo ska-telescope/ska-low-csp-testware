7679a79d8dc62bda221bf4fd8b310187e1875cca
Sander Ploegsma <sander.ploegsma@topic.nl>
2024-03-19 20:43:52 +0100

    TOP-938: Use rglob instead of walk

--- src/ska_low_csp_testware/pcap_reader_device.py
+++ src/ska_low_csp_testware/pcap_reader_device.py
@@ -91,11 +91,8 @@ class PcapReader(Device):
 
     async def _process_existing_files(self) -> None:
         path = Path(self.pcap_dir)
-        for root, _, file_names in path.walk():  # pylint: disable=no-member
-            for file_name in file_names:
-                file = root / file_name
-                if _is_monitored_file(file):
-                    await self._update_file(file)
+        for file in path.rglob("*.pcap"):
+            await self._update_file(file)
 
     async def _start_monitoring(self) -> None:
         await self._process_existing_files()
