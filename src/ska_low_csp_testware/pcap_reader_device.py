54aa5aa40fc7ebb639058e639ac46187a292b1e9
Sander Ploegsma <sander.ploegsma@topic.nl>
2024-03-25 12:32:50 +0100

    TOP-938: Use separate attributes for each file contents

--- src/ska_low_csp_testware/pcap_reader_device.py
+++ src/ska_low_csp_testware/pcap_reader_device.py
@@ -2,6 +2,8 @@
 Module for the ``PcapReader`` TANGO device.
 """
 
+import base64
+import hashlib
 import io
 import json
 import logging
@@ -10,12 +12,13 @@ import time
 from concurrent.futures import Future, ThreadPoolExecutor
 from functools import partial
 from pathlib import Path
+from typing import Any
 
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
 from ska_control_model import TestMode
-from tango import AttrQuality, AttrWriteType, DevState, DispLevel
+from tango import AttrQuality, AttrWriteType, DevFailed, DevState
 from tango.server import Device, attribute, command, device_property, run
 from watchdog.events import (
     EVENT_TYPE_CREATED,
@@ -33,10 +36,6 @@ from ska_low_csp_testware.low_cbf_vis import read_visibilities
 __all__ = ["PcapReader", "main"]
 
 
-def _is_monitored_file(path: Path):
-    return path.suffix == ".pcap"
-
-
 def _encode_dataframe(df: pd.DataFrame) -> bytes:
     buffer = io.BytesIO()
     df.to_pickle(buffer)
@@ -47,6 +46,17 @@ def _encode_ndarray(array: npt.NDArray) -> bytes:
     buffer = io.BytesIO()
     np.save(buffer, array)
     return buffer.getvalue()
+
+
+class CustomJsonEncoder(json.JSONEncoder):
+    """
+    Custom JSON encoder that allows encoding of ``bytes``.
+    """
+
+    def default(self, o: Any) -> Any:
+        if isinstance(o, bytes):
+            return base64.b64encode(o).decode("ascii")
+        return super().encode(o)
 
 
 class PcapReader(Device, FileSystemEventHandler):
@@ -67,7 +77,7 @@ class PcapReader(Device, FileSystemEventHandler):
         ),
     )
 
-    files = attribute(
+    file_name_mapping = attribute(
         doc="Attribute exposing file information for all monitored PCAP files.",
         dtype=str,
         label="PCAP files",
@@ -79,35 +89,14 @@ class PcapReader(Device, FileSystemEventHandler):
         label="Logging level",
     )
 
-    spead_headers = attribute(
-        display_level=DispLevel.EXPERT,
-        doc=(
-            "Attribute to which SPEAD headers are published. "
-            "This attribute can't be read directly as the value depends on which file is read. "
-            "Users must subscribe to USER_EVENT events instead."
-        ),
-        dtype="DevEncoded",
-        label="SPEAD headers",
-    )
-
-    spead_data = attribute(
-        display_level=DispLevel.EXPERT,
-        doc=(
-            "Attribute to which SPEAD data is published. "
-            "This attribute can't be read directly as the value depends on which file is read. "
-            "Users must subscribe to USER_EVENT events instead."
-        ),
-        dtype="DevEncoded",
-        label="SPEAD data",
-    )
-
     def __init__(self, *args, **kwargs):
         self._logger = get_logger(self, __name__)
         self._lock = threading.Lock()
         self._observer = Observer()
         self._executor = ThreadPoolExecutor()
 
-        self._files = {}
+        self._file_name_mapping = {}
+        self._dynamic_attr_data = {}
 
         super().__init__(*args, **kwargs)
 
@@ -124,12 +113,12 @@ class PcapReader(Device, FileSystemEventHandler):
         self.set_state(DevState.INIT)
         self._logger.info("Device init started")
 
-        for attribute_name in ["files"]:
+        for attribute_name in ["file_name_mapping"]:
             self.set_change_event(attribute_name, True, False)
 
         self._logger.info("Updating state to match monitored directory state")
         for file in self.pcap_dir_path.rglob("*.pcap"):
-            self._update_file(file)
+            self._sync_file_present(str(file.relative_to(self.pcap_dir_path)))
 
         if self._observer.is_alive():
             self._logger.debug("File observer already running, no need to start it again")
@@ -157,43 +146,11 @@ class PcapReader(Device, FileSystemEventHandler):
         self._logger.info("Device deinit completed")
         super().delete_device()
 
-    def _update_file(self, file: Path) -> None:
-        file_name = str(file.relative_to(self.pcap_dir_path))
-        self._logger.info("Updating file %s", file_name)
-
-        file_info = file.stat()
-
-        with self._lock:
-            self._files[file_name] = {
-                "size": file_info.st_size,
-                "mtime": file_info.st_mtime,
-            }
-            self.push_change_event("files", json.dumps(self._files))
-
-    def _remove_file(self, file: Path) -> None:
-        file_name = str(file.relative_to(self.pcap_dir_path))
-        self._logger.info("Removing file %s", file_name)
-
-        with self._lock:
-            if file_name in self._files:
-                del self._files[file_name]
-                self.push_change_event("files", json.dumps(self._files))
-
-    def on_any_event(self, event: FileSystemEvent) -> None:
-        file = Path(event.src_path)
-        if _is_monitored_file(file):
-            self._logger.debug("Received %s event for file %s", event.event_type, file)
-
-            if event.event_type in {EVENT_TYPE_CREATED, EVENT_TYPE_MODIFIED}:
-                self._update_file(file)
-            elif event.event_type == EVENT_TYPE_DELETED:
-                self._remove_file(file)
-
-    def read_files(self) -> str:
+    def read_file_name_mapping(self) -> str:
         """
-        Read method for the ``files`` device attribute.
+        Read method for the ``file_name_mapping`` device attribute.
         """
-        return json.dumps(self._files)
+        return json.dumps(self._file_name_mapping)
 
     def read_logging_level(self) -> str:
         """
@@ -210,27 +167,16 @@ class PcapReader(Device, FileSystemEventHandler):
         self._logger.setLevel(logging_level)
         self._logger.info("Logging level set to %s", logging_level)
 
-    def read_spead_headers(self) -> tuple[str, bytes, float, AttrQuality]:
+    def read_dynamic_attr(self, attr) -> tuple[str, float, AttrQuality]:
         """
-        Read method for the ``spead_headers`` device attribute.
+        Read method for the dynamic attributes.
         """
-        return (
-            "",
-            bytes(),
-            time.time(),
-            AttrQuality.ATTR_INVALID,
-        )
+        attr_name = attr.get_name()
+        with self._lock:
+            if attr_name in self._dynamic_attr_data:
+                return self._dynamic_attr_data[attr_name], time.time(), AttrQuality.ATTR_VALID
 
-    def read_spead_data(self) -> tuple[str, bytes, float, AttrQuality]:
-        """
-        Read method for the ``spead_data`` device attribute.
-        """
-        return (
-            "",
-            bytes(),
-            time.time(),
-            AttrQuality.ATTR_INVALID,
-        )
+        return "", time.time(), AttrQuality.ATTR_INVALID
 
     @command
     def RemoveFile(self, file_name: str) -> None:  # pylint: disable=invalid-name
@@ -238,7 +184,7 @@ class PcapReader(Device, FileSystemEventHandler):
         Remove the specified file from the file system.
         """
         with self._lock:
-            if file_name not in self._files:
+            if file_name not in self._file_name_mapping:
                 raise ValueError("Unknown file")
 
         self._logger.info("Removing file %s", file_name)
@@ -250,8 +196,10 @@ class PcapReader(Device, FileSystemEventHandler):
         Read visibility data from the specified file.
         """
         with self._lock:
-            if file_name not in self._files:
+            if file_name not in self._file_name_mapping:
                 raise ValueError("Unknown file")
+
+        attr_prefix = self._file_name_mapping[file_name]
 
         self._logger.info("Reading visibility data from file %s", file_name)
         future = self._executor.submit(
@@ -260,29 +208,105 @@ class PcapReader(Device, FileSystemEventHandler):
             test_mode=TestMode(self.test_mode),
             logger=self._logger,
         )
-        future.add_done_callback(partial(self._on_visibility_data, file_name))
+        future.add_done_callback(partial(self._on_visibility_data, file_name, f"{attr_prefix}_data"))
 
-    def _on_visibility_data(self, file_name: str, data: Future[PcapFileContents]) -> None:
-        def _push_event(attr_name: str, data: bytes):
-            self._logger.debug("Pushing user event for attribute %s and file %s", attr_name, file_name)
-            self.push_event(
-                attr_name,
-                [],
-                [],
-                file_name,
-                data,
-                time.time(),
-                AttrQuality.ATTR_VALID,
+    def on_any_event(self, event: FileSystemEvent) -> None:
+        file_name = str(Path(event.src_path).relative_to(self.pcap_dir_path))
+        if not file_name.endswith(".pcap"):
+            return
+
+        self._logger.debug("Received %s event for file %s", event.event_type, file_name)
+        if event.event_type == EVENT_TYPE_CREATED:
+            self._sync_file_present(file_name)
+            self._update_file_info(file_name)
+        elif event.event_type == EVENT_TYPE_MODIFIED:
+            self._update_file_info(file_name)
+        elif event.event_type == EVENT_TYPE_DELETED:
+            self._sync_file_absent(file_name)
+
+    def _sync_file_present(self, file_name: str) -> None:
+        with self._lock:
+            if file_name in self._file_name_mapping:
+                attr_prefix = self._file_name_mapping[file_name]
+            else:
+                attr_prefix = self._attr_key(file_name)
+                self._file_name_mapping[file_name] = attr_prefix
+                self.push_change_event("file_name_mapping", json.dumps(self._file_name_mapping))
+
+        for attr_suffix in ["data", "info"]:
+            attr_name = f"{attr_prefix}_{attr_suffix}"
+            if self._attr_exists(attr_name):
+                continue
+
+            self._logger.info("Creating dynamic attribute %s", attr_name)
+            attr = attribute(name=attr_name, dtype=str, fget=self.read_dynamic_attr)
+            self.add_attribute(attr)
+            self.set_change_event(attr_name, True, False)
+
+    def _sync_file_absent(self, file_name: str) -> None:
+        with self._lock:
+            if file_name not in self._file_name_mapping:
+                return
+
+            attr_prefix = self._file_name_mapping[file_name]
+            del self._file_name_mapping[file_name]
+            self.push_change_event("file_name_mapping", json.dumps(self._file_name_mapping))
+
+            for attr_suffix in ["data", "info"]:
+                attr_name = f"{attr_prefix}_{attr_suffix}"
+                if not self._attr_exists(attr_name):
+                    continue
+
+                self._logger.info("Removing dynamic attribute %s", attr_name)
+                self.remove_attribute(attr_name)
+                if attr_name in self._dynamic_attr_data:
+                    del self._dynamic_attr_data[attr_name]
+
+    def _update_file_info(self, file_name: str) -> None:
+        self._logger.info("Updating file info %s", file_name)
+
+        file_info = self.pcap_dir_path.joinpath(file_name).stat()
+
+        with self._lock:
+            attr_name = f"{self._file_name_mapping[file_name]}_info"
+            attr_value = json.dumps(
+                {
+                    "size": file_info.st_size,
+                    "mtime": file_info.st_mtime,
+                }
             )
+            self._dynamic_attr_data[attr_name] = attr_value
+            self.push_change_event(attr_name, attr_value)
 
+    def _attr_key(self, file_name: str) -> str:
+        return hashlib.sha1(file_name.encode(), usedforsecurity=False).hexdigest()
+
+    def _on_visibility_data(self, file_name: str, attr_name: str, data: Future[PcapFileContents]) -> None:
         if e := data.exception():
             self._logger.warning("Failed reading visibility data from file %s: %s", file_name, e)
             return
 
         self._logger.info("Finished reading visibility data from file %s", file_name)
         file_contents = data.result()
-        _push_event("spead_headers", _encode_dataframe(file_contents.spead_headers))
-        _push_event("spead_data", _encode_ndarray(file_contents.spead_data))
+        attr_value = json.dumps(
+            {
+                "headers": _encode_dataframe(file_contents.spead_headers),
+                "averaged_data": _encode_ndarray(file_contents.spead_data),
+            },
+            cls=CustomJsonEncoder,
+        )
+
+        self.push_change_event(attr_name, attr_value, time.time(), AttrQuality.ATTR_VALID)
+
+        with self._lock:
+            self._dynamic_attr_data[attr_name] = attr_value
+
+    def _attr_exists(self, attr_name: str) -> bool:
+        try:
+            self.get_attribute_config([attr_name])
+            return True
+        except DevFailed:
+            return False
 
 
 def main(args=None, **kwargs):  # pylint: disable=missing-function-docstring
--- tests/pcap_reader_device_test.py
+++ tests/pcap_reader_device_test.py
@@ -16,17 +16,21 @@ from ska_low_csp_testware.pcap_reader_device import PcapReader
 pytestmark = pytest.mark.forked
 
 
+@pytest.fixture(name="existing_pcap_file_name")
+def fxt_existing_pcap_file_name():
+    return "existing_pcap_file.pcap"
+
+
 @pytest.fixture(name="pcap_dir")
-def fxt_pcap_dir(tmp_path: Path):
-    tmp_path.joinpath("existing_pcap_file.pcap").touch()
-    tmp_path.joinpath("existing_txt_file.txt").touch()
+def fxt_pcap_dir(tmp_path: Path, existing_pcap_file_name: str):
+    tmp_path.joinpath(existing_pcap_file_name).touch()
     return tmp_path
 
 
 @pytest.fixture(name="event_callbacks")
 def fxt_event_callbacks():
     return MockTangoEventCallbackGroup(
-        "files",
+        "file_name_mapping",
         "spead_data",
         "spead_headers",
     )
@@ -45,51 +49,92 @@ def fxt_device(pcap_dir: Path):
         yield device
 
 
+def test_files_attribute_contains_existing_files_after_init(
+    device: DeviceProxy,
+    existing_pcap_file_name: str,
+):
+    assert existing_pcap_file_name in json.loads(device.file_name_mapping)
+
+
+def test_dynamic_attributes_are_created_for_existing_files_after_init(
+    device: DeviceProxy,
+    existing_pcap_file_name: str,
+):
+    attr_prefix = json.loads(device.file_name_mapping)[existing_pcap_file_name]
+    assert f"{attr_prefix}_data" in device.get_attribute_list()
+    assert f"{attr_prefix}_info" in device.get_attribute_list()
+
+
+def test_files_attribute_does_not_contain_file_when_deleted(
+    device: DeviceProxy,
+    pcap_dir: Path,
+    existing_pcap_file_name: str,
+):
+    file = pcap_dir.joinpath(existing_pcap_file_name)
+    file.unlink()
+    time.sleep(0.5)
+
+    assert existing_pcap_file_name not in json.loads(device.file_name_mapping)
+
+
+def test_dynamic_attributes_are_removed_when_file_is_deleted(
+    device: DeviceProxy,
+    pcap_dir: Path,
+    existing_pcap_file_name: str,
+):
+    attr_prefix = json.loads(device.file_name_mapping)[existing_pcap_file_name]
+
+    file = pcap_dir.joinpath(existing_pcap_file_name)
+    file.unlink()
+    time.sleep(0.5)
+
+    assert f"{attr_prefix}_data" not in device.get_attribute_list()
+    assert f"{attr_prefix}_info" not in device.get_attribute_list()
+
+
 def test_files_attribute_reacts_to_created_pcap_file(
     pcap_dir: Path,
     device: DeviceProxy,
     event_callbacks: MockTangoEventCallbackGroup,
 ):
-    existing_files = device.files
+    existing_files = device.file_name_mapping
 
     device.subscribe_event(
-        "files",
+        "file_name_mapping",
         EventType.CHANGE_EVENT,
-        event_callbacks["files"],
+        event_callbacks["file_name_mapping"],
     )
 
     time.sleep(0.5)
-    event_callbacks["files"].assert_change_event(existing_files)
+    event_callbacks["file_name_mapping"].assert_change_event(existing_files)
 
     pcap_file = pcap_dir / "new_pcap_file.pcap"
     pcap_file.touch()
     time.sleep(0.5)
 
-    assert pcap_file.name in json.loads(device.files)
-    event_callbacks["files"].assert_change_event(device.files)
+    assert pcap_file.name in json.loads(device.file_name_mapping)
+    event_callbacks["file_name_mapping"].assert_change_event(device.file_name_mapping)
 
     pcap_file.unlink()
     time.sleep(0.5)
 
-    assert pcap_file.name not in json.loads(device.files)
-    event_callbacks["files"].assert_change_event(device.files)
+    assert pcap_file.name not in json.loads(device.file_name_mapping)
+    event_callbacks["file_name_mapping"].assert_change_event(device.file_name_mapping)
 
 
+@pytest.mark.debug
 def test_read_visibilities(
     device: DeviceProxy,
-    event_callbacks: MockTangoEventCallbackGroup,
+    existing_pcap_file_name: str,
 ):
-    expected_event_callbacks = ["spead_data", "spead_headers"]
+    attr_prefix = json.loads(device.file_name_mapping)[existing_pcap_file_name]
+    attr_name = f"{attr_prefix}_data"
+    callbacks = MockTangoEventCallbackGroup(attr_name)
+    device.subscribe_event(attr_name, EventType.CHANGE_EVENT, callbacks[attr_name])
+    time.sleep(0.5)
+    callbacks[attr_name].assert_change_event(None)
 
-    for attr_name in expected_event_callbacks:
-        device.subscribe_event(attr_name, EventType.USER_EVENT, event_callbacks[attr_name])
-
-    pcap_file_name = "existing_pcap_file.pcap"
-    device.ReadVisibilityData(pcap_file_name)
+    device.ReadVisibilityData(existing_pcap_file_name)
     time.sleep(0.5)
 
-    for attr_name in expected_event_callbacks:
-        event_callbacks[attr_name].assert_change_event(
-            (pcap_file_name, placeholders.Anything),
-            lookahead=2,
-        )
+    callbacks[attr_name].assert_change_event(placeholders.Anything)
